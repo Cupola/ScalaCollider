8b6d61acd160f08db5bec333999910ef75437add
Sciss <contact@sciss.de>
2010-06-13 11:21:38 +0100

    scala 2.8.0.RC4. now SynthDef(...).play works, but new symbol-not-found compiler errors occur. namely, {...}.play doesn't produce a proper release envelope wrapping any more :-(

--- project/build/ScalaCollider.scala
+++ project/build/ScalaCollider.scala
@@ -2,8 +2,8 @@ import sbt._
 import java.io.{ IOException, RandomAccessFile }
 
 class ScalaColliderProject( info: ProjectInfo ) extends DefaultProject( info ) { 
-   val dep1 = "de.sciss" %% "scalaosc" % "0.15"
-   val dep2 = "de.sciss" %% "scalaaudiofile" % "0.11"
+   val dep1 = "de.sciss" %% "scalaosc" % "0.16"
+   val dep2 = "de.sciss" %% "scalaaudiofile" % "0.12"
    lazy val demo = demoAction
 
    protected def demoAction = {
