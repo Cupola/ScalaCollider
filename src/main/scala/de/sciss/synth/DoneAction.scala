a5aae1e23633ed9a40e85a4a0739ac69b695b3a1
Sciss <contact@sciss.de>
2010-07-05 12:09:10 +0100

    doc for doneActions

--- src/main/scala/de/sciss/synth/DoneAction.scala
+++ src/main/scala/de/sciss/synth/DoneAction.scala
@@ -30,18 +30,85 @@ package de.sciss.synth
 
 sealed abstract class DoneAction( val id: Int )
 
+/**
+ *    Does nothing in particular when the UGen is finished
+ */
 case object doNothing         extends DoneAction( 0 )
+/**
+ *    Pauses the enclosing synth when the UGen is finished
+ */
 case object pauseSelf         extends DoneAction( 1 )
+/**
+ *    Frees the enclosing synth when the UGen is finished
+ */
 case object freeSelf          extends DoneAction( 2 )
+/**
+ *    Frees the enclosing synth as well as the predecessor node
+ *    when the UGen is finished
+ */
 case object freeSelfPred      extends DoneAction( 3 )
+/**
+ *    Frees the enclosing synth as well as the successor node
+ *    when the UGen is finished
+ */
 case object freeSelfSucc      extends DoneAction( 4 )
+/**
+ *    Frees the enclosing synth when the UGen is finished.
+ *    If the predecessor node is a group, calls freeAll on
+ *    that group. If the predecssor node is a synth,
+ *    frees that synth.
+ */
 case object freeSelfPredAll   extends DoneAction( 5 )
+/**
+ *    Frees the enclosing synth when the UGen is finished.
+ *    If the successor node is a group, calls freeAll on
+ *    that group. If the successor node is a synth,
+ *    frees that synth.
+ */
 case object freeSelfSuccAll   extends DoneAction( 6 )
+/**
+ *    Frees the enclosing synth and all preceding nodes
+ *    in its group when the UGen is finished
+ */
 case object freeSelfToHead    extends DoneAction( 7 )
+/**
+ *    Frees the enclosing synth and all succeeding nodes
+ *    in its group when the UGen is finished
+ */
 case object freeSelfToTail    extends DoneAction( 8 )
+/**
+ *    Frees the enclosing synth and pauses the predecessor node
+ *    when the UGen is finished
+ */
 case object freeSelfPausePred extends DoneAction( 9 )
+/**
+ *    Frees the enclosing synth and pauses the successor node
+ *    when the UGen is finished
+ */
 case object freeSelfPauseSucc extends DoneAction( 10 )
+/**
+ *    Frees the enclosing synth when the UGen is finished.
+ *    If the predecessor node is a group, calls deepFree on
+ *    that group. If the predecessor node is a synth,
+ *    frees that synth.
+ */
 case object freeSelfPredDeep  extends DoneAction( 11 )
+/**
+ *    Frees the enclosing synth when the UGen is finished.
+ *    If the successor node is a group, calls deepFree on
+ *    that group. If the successor node is a synth,
+ *    frees that synth.
+ */
 case object freeSelfSuccDeep  extends DoneAction( 12 )
+/**
+ *    Frees the enclosing synth along with all other nodes
+ *    in the group when the UGen is finished (i.e. does
+ *    a freeAll on the group)
+ */
 case object freeAllInGroup    extends DoneAction( 13 )
+/**
+ *    Frees the enclosing group when the UGen is finished,
+ *    and hence also frees this synth along with all other
+ *    nodes in the group.
+ */
 case object freeGroup         extends DoneAction( 14 )
